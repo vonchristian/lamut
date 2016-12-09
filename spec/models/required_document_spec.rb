e7a807526dc1a31c5d76b420a9c5d281d87f263e
Von Christian Halip <vc.halip@gmail.com>
2016-12-09 12:07:23 +0800

    Add test to required_document model.

--- spec/models/required_document_spec.rb
+++ spec/models/required_document_spec.rb
@@ -7,5 +7,24 @@ RSpec.describe RequiredDocument, type: :model do
     it { is_expected.to have_many :issuances }
   end
   describe "delegations" do
+    it do
+      is_expected.to delegate_method(:name).to(:department).with_prefix
+    end
   end
+  it ".default" do
+    required_document1 = create(:required_document, default: true)
+    required_document2 = create(:required_document, default: false)
+
+    expect(RequiredDocument.default).to include(required_document1)
+    expect(RequiredDocument.default).to_not include(required_document2)
+  end
+
+  it ".not_default" do
+    required_document1 = create(:required_document, default: true)
+    required_document2 = create(:required_document, default: false)
+
+    expect(RequiredDocument.not_default).to include(required_document2)
+    expect(RequiredDocument.not_default).to_not include(required_document1)  
+  end
+
 end
